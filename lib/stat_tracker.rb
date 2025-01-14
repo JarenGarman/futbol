c0d47180606149f363a4b1689e85263fd9c67850
Will Fox <william.macdonald.fox@gmail.com>
2025-01-14 17:39:00 -0500

    feat: add team creation framework, successful reading of data thus far

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,9 +1,21 @@
 require 'csv'
 require 'pry'
+require './lib/team'
 
 class StatTracker
-
+    
     def self.from_csv(locations)
-    binding.pry
+        # instance = self.new
+
+        teams = []
+
+        CSV.foreach(locations[:teams], headers: true, header_converters: :symbol) do |row|
+            teams << Team.new(row[:team_id], row[:teamname])
+        end
+
+        binding.pry
+
+
+        # instance 
     end
 end 
