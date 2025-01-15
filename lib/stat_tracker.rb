b2e170fb72b0cb1afb4a7f4479c622d788d60a99
Will Fox <william.macdonald.fox@gmail.com>
2025-01-14 21:25:20 -0500

    feat: add to parsing method to include games and games_by_team

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -23,9 +23,34 @@ class StatTracker
             teams << Team.new(row[:team_id], row[:teamname])
         end
 
+        games = []
+
+        CSV.foreach(locations[:games], headers: true, header_converters: :symbol) do |row|
+            games << Game.new(
+                row[:game_id],
+                row[:season],
+                row[:away_team_id],
+                row[:home_team_id],
+                row[:away_goals],
+                row[:home_goals]
+            )
+        end
+
+        game_teams = []
+
+        CSV.foreach(locations[:game_teams], headers: true, header_converters: :symbol) do |row|
+            game_teams << GameByTeam.new(
+                row[:game_id],
+                row[:team_id],
+                row[:hoa],
+                row[:result],
+                row[:head_coach],
+                row[:goals],
+                row[:shots],
+                row[:tackles]
+            )
+        end
+
         binding.pry
-
-
-        # instance
     end
 end 
