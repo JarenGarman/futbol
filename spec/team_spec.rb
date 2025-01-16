8a254394736145eb615fb06478326686ac9c7a4f
Jaren Garman <jarengarman@protonmail.com>
2025-01-16 12:35:10 -0500

    chore: Fix indentation size in team_spec

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,17 +1,17 @@
 require_relative 'spec_helper'
 
 RSpec.describe Team do
-  subject(:team) { Team.new('23', 'Reale Pomodoros') }
+    subject(:team) { Team.new('23', 'Reale Pomodoros') }
 
-  describe '#initialize' do
-    it { is_expected.to be_instance_of Team }
+    describe '#initialize' do
+        it { is_expected.to be_instance_of Team }
 
-    it 'has an id' do
-      expect(team.id).to eq('23')
+        it 'has an id' do
+            expect(team.id).to eq('23')
+        end
+
+        it 'has a name' do
+            expect(team.name).to eq('Reale Pomodoros')
+        end
     end
-
-    it 'has a name' do
-      expect(team.name).to eq('Reale Pomodoros')
-    end
-  end
 end
