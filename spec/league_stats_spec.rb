8797989f9f8f1e48285e8e68a99e32d47efb054a
Jaren Garman <jarengarman@protonmail.com>
2025-01-16 19:00:47 -0500

    test: reorganize league_stats_spec and add test for teams

--- spec/league_stats_spec.rb
+++ spec/league_stats_spec.rb
@@ -1,13 +1,23 @@
 require_relative 'spec_helper'
 
 RSpec.describe LeagueStats do
-    subject(:league_stats) { LeagueStats.new([], []) }
+    subject(:league_stats) do
+      StatTracker.from_csv({
+                             games: './data/games.csv',
+                             teams: './data/teams.csv',
+                             game_teams: './data/game_teams.csv'
+                           }).league_stats
+    end
 
     describe '#initialize' do
         it { is_expected.to be_instance_of LeagueStats }
-    end
 
-    it 'has games' do
-        expect(league_stats.games).to be_an(Array)
+        it 'has teams' do
+            expect(league_stats.teams.all?(Team)).to be true
+        end
+
+        it 'has games' do
+            expect(league_stats.games.all?(Game)).to be true
+        end
     end
 end
