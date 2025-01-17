f445dae4afe2540a4a2e8d76fc34393a7f3d46af
Will Fox <william.macdonald.fox@gmail.com>
2025-01-17 16:57:09 -0500

    feat: implement average_goals_by_season

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -57,4 +57,31 @@ class GameStats
         average_goals = total_goals / @games.count.to_f 
         average_goals.round(2)
     end
+
+    #refactor later with .each_with_object({}) do ?
+    def average_goals_by_season
+        seasons = @games.map do |game|
+            game.season 
+        end.uniq
+        games_by_season = {}
+            seasons.each do |season|
+                season_games = []
+                @games.each do |game|
+                    season_games << game if game.season == season 
+                end
+                games_by_season[season] = season_games
+            end
+
+        avg_goals_by_season = {}
+        games_by_season.each do |season, season_games|
+            total_games_in_season = season_games.count
+            total_goals_in_season = 0
+            season_games.each do |game|
+                total_goals_in_season += (game.away_goals + game.home_goals)
+            end
+            average = (total_goals_in_season / total_games_in_season.to_f).round(2)
+            avg_goals_by_season[season] = average
+        end
+        avg_goals_by_season
+    end
 end
