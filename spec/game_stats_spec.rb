5f68c257e17837c60e40544e96f48755ada4d3ef
Jaren Garman <jarengarman@protonmail.com>
2025-01-18 14:35:38 -0500

    test: format count_of_games_by_season and average_goals_by_season

--- spec/game_stats_spec.rb
+++ spec/game_stats_spec.rb
@@ -33,38 +33,35 @@ RSpec.describe GameStats do
         it '#percentage_visitor_wins' do
             expect(game_stats.percentage_visitor_wins).to eq(0.36)
         end
-       
-        it '#percentage_ties' do 
+
+        it '#percentage_ties' do
             expect(game_stats.percentage_ties).to eq(0.20)
         end
-    
 
-        it "#count_of_games_by_season" do
-            expected = {
-            "20122013"=>806,
-            "20162017"=>1317,
-            "20142015"=>1319,
-            "20152016"=>1321,
-            "20132014"=>1323,
-            "20172018"=>1355
-            }
-            expect(game_stats.count_of_games_by_season).to eq(expected)
+        it '#count_of_games_by_season' do
+            expect(game_stats.count_of_games_by_season).to eq({
+                                                                '20122013' => 806,
+                                                                '20162017' => 1317,
+                                                                '20142015' => 1319,
+                                                                '20152016' => 1321,
+                                                                '20132014' => 1323,
+                                                                '20172018' => 1355
+                                                              })
         end
 
-        it "#average_goals_per_game" do 
+        it '#average_goals_per_game' do
             expect(game_stats.average_goals_per_game).to eq(4.22)
         end
 
-        it "#average_goals_by_season" do 
-            expected = {
-            "20122013"=>4.12,
-            "20162017"=>4.23,
-            "20142015"=>4.14,
-            "20152016"=>4.16,
-            "20132014"=>4.19,
-            "20172018"=>4.44
-            }
-            expect(game_stats.average_goals_by_season).to eq(expected)
+        it '#average_goals_by_season' do
+            expect(game_stats.average_goals_by_season).to eq({
+                                                               '20122013' => 4.12,
+                                                               '20162017' => 4.23,
+                                                               '20142015' => 4.14,
+                                                               '20152016' => 4.16,
+                                                               '20132014' => 4.19,
+                                                               '20172018' => 4.44
+                                                             })
         end
     end
 end
