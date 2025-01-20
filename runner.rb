962b7a1eb34ef7e9905bdf6b9d3dc921ab0c4f29
Jaren Garman <jarengarman@protonmail.com>
2025-01-20 15:12:59 -0500

    chore: fix formatting in runner

--- runner.rb
+++ runner.rb
@@ -1,16 +1,10 @@
-require_relative '../lib/stat_tracker'
-
-game_path = './data/games.csv'
-team_path = './data/teams.csv'
-game_teams_path = './data/game_teams.csv'
-
-locations = {
-  games: game_path,
-  teams: team_path,
-  game_teams: game_teams_path
-}
-
-stat_tracker = StatTracker.from_csv(locations)
-
+require_relative 'lib/stat_tracker'
 require 'pry'
+
+stat_tracker = StatTracker.from_csv({
+                                      games: './data/games.csv',
+                                      teams: './data/teams.csv',
+                                      game_teams: './data/game_teams.csv'
+                                    })
+
 binding.pry
